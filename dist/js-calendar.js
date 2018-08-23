997b3de9c458f12db9b97c672a1da035e5306c7d
James <jtmacon36@gmail.com>
2018-08-23 17:26:09 -0400

    script compiling with babelify so that exports work

--- dist/js-calendar.js
+++ dist/js-calendar.js
@@ -1,4 +1,9 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,7 +13,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _jscallog(msg) {
+var _jscallog = function _jscallog(msg) {
     console.log("[JSCalendar - " + new Date().toLocaleTimeString() + "] " + msg);
 };
 
@@ -1299,3 +1304,7 @@ var JSCalendar = function () {
 
 JSCalendar.hooks = {};
 
+exports.JSCalendar = JSCalendar;
+exports.JSCalendarEvent = JSCalendarEvent;
+
+},{}]},{},[1]);
